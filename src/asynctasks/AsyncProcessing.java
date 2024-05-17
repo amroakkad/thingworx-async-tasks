c5e569c2fe8f5f8eb01ebaf0d5019319e4e75806
Administrator <Administrator@192.168.2.47>
2024-05-17 13:34:37 +0200

    Removed, ignored relevant parts.

--- src/asynctasks/AsyncProcessing.java
+++ src/asynctasks/AsyncProcessing.java
@@ -47,120 +47,18 @@ public class AsyncProcessing {
 	public AsyncProcessing() {
 	}
 
-	@ThingworxServiceDefinition(name = "ProcessTasks", description = "Process the given tasks in parallel. Blocking. Will return when the slowest task will finish.", category = "", isAllowOverride = false, aspects = {
-			"isAsync:false" })
-	@ThingworxServiceResult(name = "Result", description = "", baseType = "INFOTABLE", aspects = {
-			"isEntityDataShape:true", "dataShape:PTC.AT.Output.Datashape" })
-	public InfoTable ProcessTasks(
-			@ThingworxServiceParameter(name = "Tasks", description = "", baseType = "INFOTABLE", aspects = {
-					"isEntityDataShape:true", "dataShape:PTC.AT.Input.Datashape" }) InfoTable Tasks)
-			throws Exception {
-		_logger.trace("Entering Service: ProcessTasks");
-//1. Create an output Infotable as results placeholder
-		InfoTable iftbl_TaskOutput = InfoTableInstanceFactory.createInfoTableFromDataShape("PTC.AT.Output.Datashape");
-//2. Record the current security context to pass to the Completable Future, as they don't inherit the current context
-		SecurityContext currentSecurityCtx = ThreadLocalContext.getSecurityContext();
-		Integer intTaskCount = Tasks.getRowCount();
-//create a new ArrayList of CompletableFuture as placeholder for all Tasks supplied in the Tasks infotable
-		ArrayList<CompletableFuture<ValueCollection>> tasks = new ArrayList<CompletableFuture<ValueCollection>>();
-		if (intTaskCount != 0) {
-			// 3. Loop through the Task list and create a Completable Future object for each
-			// task
-			for (Iterator<ValueCollection> iterator = Tasks.getRows().iterator(); iterator.hasNext();) {
-				// 3.1 Iterate through the current Task attributes and store them in local
-				// variables
-				ValueCollection vcTask = (ValueCollection) iterator.next();
-				String strThingName = vcTask.getStringValue("ThingName");
-				String strServiceName = vcTask.getStringValue("Service");
-				String strInputParameters = vcTask.getStringValue("InputParameters");
-				if (strInputParameters == null)
-					strInputParameters = "";
-				String finalInputParameters = strInputParameters;
-				// 3.2. We convert the InputParameters to a JSON object to be able to easier add
-				// them as input parameters to the service
-				ObjectMapper mappr = new ObjectMapper();
-				mappr.setSerializationInclusion(Include.NON_NULL);
-				JsonNode jsonInputParameters = mappr.readTree(finalInputParameters);
-				Iterator<String> fieldNames = jsonInputParameters.fieldNames();
-				ValueCollection inputParameters = new ValueCollection();
-				while (fieldNames.hasNext()) {
-					String fieldName = fieldNames.next();
-					inputParameters.put(fieldName, new StringPrimitive(jsonInputParameters.get(fieldName).asText()));
-				}
-				// at this step all the input parameters are stored in the format required for
-				// the processServiceRequest format
-				// 3.3. We create a CompletableFuture with return type of ValueCollection, as
-				// needed by the infotable at Step 1
-				CompletableFuture<ValueCollection> future = CompletableFuture.supplyAsync(() -> {
-					ValueCollection vc = new ValueCollection();
-					try {
-						// 3.3.1. Set the Security Context to the current extension security Context
-						// since this will not execute in the same thread
-						ThreadLocalContext.setSecurityContext(currentSecurityCtx);
-						// 3.3.2 Find the Thing specified in the ThingName parameter at step 3.1
-						Thing thing = (Thing) EntityUtilities.findEntity(strThingName,
-								ThingworxRelationshipTypes.Thing);
-						InfoTable iftbl_Result;
-						// 3.3.3. If the service has no input parameters we pass a null
-						if (inputParameters.size() == 0)
-							iftbl_Result = thing.processServiceRequest(strServiceName, null);
-						else
-							iftbl_Result = thing.processServiceRequest(strServiceName, inputParameters);
-						// 3.3.4. We no longer need a ThingWorx security context here, so we clear it.
-						ThreadLocalContext.clearSecurityContext();
-						// 3.3.5 We create the ValueCollection (the result row) that will be added in
-						// the Infotable at Step 1
-						vc.put("ThingName", new StringPrimitive(strThingName));
-						vc.put("Service", new StringPrimitive(strServiceName));
-						vc.put("InputParameters", new StringPrimitive(finalInputParameters));
-						// 3.3.6. The extension currently supports only methods with output set as
-						// String
-						// convert any infotables to String, and convert them back as needed in the
-						// caller service
-						vc.put("Output", new StringPrimitive(iftbl_Result.getRow(0).toString()));
-					} catch (Exception e) {
-						_logger.error("Thing: " + strThingName + "| Service: " + strServiceName + " | InputParameters: "
-								+ finalInputParameters + " | Message: " + e.getMessage());
-					}
-					return vc;
-				});
-				tasks.add(future);
-				_logger.trace("Exiting Service: ProcessTasks");
-			}
-			// 4. All Tasks have been added to the ArrayList, it's now time to execute them.
-			// This is the step that will block for an amount of time equal to the slowest
-			// task execution.
-			CompletableFuture.allOf(tasks.toArray(new CompletableFuture[intTaskCount])).thenRun(() -> {
-				try {
-					// 4.1 Each task is being executed in parallel and once finished it will add its
-					// string result to the infotable at Step 1
-					for (CompletableFuture<ValueCollection> completableFuture : tasks) {
-						iftbl_TaskOutput.addRow(completableFuture.get());
-					}
-				} catch (InterruptedException e) {
-					_logger.error("Interrupted exception: " + e.getStackTrace().toString());
-				} catch (ExecutionException e) {
-					_logger.error("Execution exception: " + e.getStackTrace().toString());
-				}
-
-			}).join();
-		}
-		return iftbl_TaskOutput;
-	}
-
 	@SuppressWarnings("rawtypes")
 	@ThingworxServiceDefinition(name = "XProcessTasks", description = "", category = "", isAllowOverride = false, aspects = {
 			"isAsync:false" })
 	@ThingworxServiceResult(name = "Result", description = "", baseType = "INFOTABLE", aspects = {
 			"isEntityDataShape:true", "dataShape:PTC.AT.XOutput.Datashape" })
-	public InfoTable XProcessTasks(
+	public InfoTable ProcessTasks(
 			@ThingworxServiceParameter(name = "Tasks", description = "Extended Processing of Tasks. Supporting all types for input parameters and output parameters.", baseType = "INFOTABLE", aspects = {
 					"isRequired:true", "isEntityDataShape:true", "dataShape:PTC.AT.XInputDatashape" }) InfoTable Tasks)
 			throws Exception {
 		_logger.trace("Entering Service: XProcessTasks");
 		// 1. Create an output Infotable as results placeholder
-		InfoTable iftbl_TaskOutput = InfoTableInstanceFactory
-				.createInfoTableFromDataShape("PTC.AT.XOutput.Datashape");
+		InfoTable iftbl_TaskOutput = InfoTableInstanceFactory.createInfoTableFromDataShape("PTC.AT.XOutput.Datashape");
 
 		// 2. Record the current security context to pass to the Completable Future, as
 		// they don't inherit the current context
@@ -169,13 +67,17 @@ public class AsyncProcessing {
 		Integer intTaskCount = Tasks.getRowCount();
 
 		if (intTaskCount != 0) {
-			
+
+			// create a new ArrayList of CompletableFuture as placeholder for all Tasks
+			// supplied in the Tasks infotable
 			ArrayList<CompletableFuture<ValueCollection>> tasks = new ArrayList<CompletableFuture<ValueCollection>>();
 
+			// 3. Loop through the Task list and create a Completable Future object for each task
 			for (ValueCollection row : Tasks.getRows()) {
-				
+
+				// 3.1 Iterate through the current Task attributes and store them in local variables
 				ValueCollection serviceParameters = new ValueCollection();
-				
+
 				InfoTable itp = InfoTableInstanceFactory
 						.createInfoTableFromDataShape("PTC.AT.XInputParameters.Datashape");
 
@@ -184,48 +86,56 @@ public class AsyncProcessing {
 				String outputBaseType = row.getStringValue("OutputBaseType");
 				String outputEventTopic = row.getStringValue("OutputEventTopic");
 
+				//3.2 If available, handle input parameters
 				if (row.has("InputParameters")) {
 					InfoTable itInputParameters = (InfoTable) row.getPrimitive("InputParameters").getValue();
 
 					for (ValueCollection parameter : itInputParameters.getRows()) {
 						String paramName = parameter.getStringValue("Name");
 						String paramBaseType = parameter.getStringValue("BaseType");
-						
-						IPrimitiveType serviceParamValue;
-						
-						if(paramBaseType.equals("INFOTABLE")) {
-							InfoTable paramComplexValue = (InfoTable) parameter.getValue("ComplexValue");							
-							serviceParamValue = new InfoTablePrimitive(paramComplexValue);							
-						}
-						else {
-							String paramValue = parameter.getStringValue("Value");	
-							serviceParamValue = extractValue(paramName, paramBaseType, paramValue);							
-						}						
 
-						if (serviceParamValue != null /* && serviceParameters.size()>0 */ ) { // null = BaseType NOTHING, otherwise exception is thrown
+						IPrimitiveType serviceParamValue;
+
+						if (paramBaseType.equals("INFOTABLE")) {
+							InfoTable paramComplexValue = (InfoTable) parameter.getValue("ComplexValue");
+							serviceParamValue = new InfoTablePrimitive(paramComplexValue);
+						} else {
+							String paramValue = parameter.getStringValue("Value");
+							serviceParamValue = extractValue(paramName, paramBaseType, paramValue);
+						}
+
+						if (serviceParamValue != null /* && serviceParameters.size()>0 */ ) { // null = BaseType
+																								// NOTHING, otherwise
+																								// exception is thrown
 							serviceParameters.put(paramName, serviceParamValue);
 							itp.addRow(parameter);
 						}
-						
-						
 
 					}
 
 				}
 
-				// at this stage all input parameters are ready
+				//Note: at this stage all input parameters are ready
+				//3.3 CompletableFuture with return type of ValueCollection, as needed by the info table at Step 1
 				CompletableFuture<ValueCollection> future = CompletableFuture.supplyAsync(() -> {
+					// 3.3.1 We create the ValueCollection (the result row) that will be added in info table at Step 1
 					ValueCollection vc = new ValueCollection();
 
+					// 3.3.2. Set the Security Context to the current extension security Context
+					// since this will not execute in the same thread
 					ThreadLocalContext.setSecurityContext(currentSecurityCtx);
 
+					// 3.3.3 Find the Thing specified in the ThingName parameter at step 3.1
 					Thing thing = ThingUtilities.findThing(strThingName);
 
 					InfoTable iftbl_Result;
 					try {
+						// 3.3.4. If the service has no input parameters we pass a null
 						iftbl_Result = (serviceParameters.size() == 0)
 								? thing.processServiceRequest(strServiceName, null)
 								: thing.processServiceRequest(strServiceName, serviceParameters);
+						
+						// 3.3.5. We no longer need a ThingWorx security context here, so we clear it.
 						ThreadLocalContext.clearSecurityContext();
 
 						vc.put("ThingName", new StringPrimitive(strThingName));
@@ -234,16 +144,16 @@ public class AsyncProcessing {
 						vc.put("InputParameters", new InfoTablePrimitive(itp));
 						vc.put("OutputBaseType", new StringPrimitive(outputBaseType));
 						vc.put("OutputEventTopic", new StringPrimitive(outputEventTopic));
-
-						if (iftbl_Result != null && iftbl_Result.getRowCount() > 0) {							
-							if(outputBaseType.equals("INFOTABLE")) {
+						
+						//3.3.6 Return based on outputBaseType complex response or simple one.
+						if (iftbl_Result != null && iftbl_Result.getRowCount() > 0) {
+							if (outputBaseType.equals("INFOTABLE")) {
 								vc.put("OutputComplexResult", new InfoTablePrimitive(iftbl_Result));
-							}
-							else {
+							} else {
 								IPrimitiveType result = extractResult(iftbl_Result, outputBaseType);
 								vc.put("OutputResult", result);
 							}
-							
+
 						}
 
 					} catch (Exception e) {
@@ -256,10 +166,13 @@ public class AsyncProcessing {
 				_logger.trace("Exiting Service: XProcessTasks");
 			}
 
+			// 4. All Tasks have been added to the ArrayList, it's now time to execute them.
+			// This is the step that will block for an amount of time equal to the slowest
+			// task execution.
 			CompletableFuture.allOf(tasks.toArray(new CompletableFuture[intTaskCount])).thenRun(() -> {
 				try {
 					// 4.1 Each task is being executed in parallel and once finished it will add its
-					// string result to the infotable at Step 1
+					// string result to the info table at Step 1
 					for (CompletableFuture<ValueCollection> completableFuture : tasks) {
 						iftbl_TaskOutput.addRow(completableFuture.get());
 					}
@@ -271,29 +184,28 @@ public class AsyncProcessing {
 			}).join();
 		}
 
+		//4.2 Finally return info table of tasks.
 		return iftbl_TaskOutput;
 	}
 
 	@SuppressWarnings({ "rawtypes", "deprecation" })
 	private IPrimitiveType extractResult(InfoTable iftbl_Result, String outputBaseType) throws Exception {
-		
+
 		Object resultObj = iftbl_Result.getRow(0).getValue("result");
 
 		switch (outputBaseType) {
 		case "BOOLEAN":
 			return new BooleanPrimitive((Boolean) resultObj);
 		case "DOUBLE":
-			return new NumberPrimitive((Double) resultObj);			
+			return new NumberPrimitive((Double) resultObj);
 		case "INTEGER":
 			return new IntegerPrimitive((Integer) resultObj);
 		case "LONG":
 			return new LongPrimitive((Long) resultObj);
-		case "NOTHING":
-			return new StringPrimitive("NOTHING");
 		case "STRING":
 			return new StringPrimitive(resultObj.toString());
 //			break;
-		default:
+		default: // "NOTHING"
 			String errMsg = AsyncProcessing.class.getSimpleName() + " output has unknown base type " + outputBaseType
 					+ ".";
 			_logger.error(errMsg);
@@ -313,11 +225,9 @@ public class AsyncProcessing {
 			return new IntegerPrimitive(Integer.parseInt(paramValue));
 		case "LONG":
 			return new LongPrimitive(Long.parseLong(paramValue));
-		case "NOTHING":
-			return null;
 		case "STRING":
 			return new StringPrimitive(paramValue);
-		default:
+		default: // "NOTHING"
 			String errMsg = AsyncProcessing.class.getSimpleName() + " Parameter " + paramName
 					+ " has assigned unknown base type " + paramBaseType + ".";
 			_logger.error(errMsg);
@@ -325,11 +235,5 @@ public class AsyncProcessing {
 		}
 
 	}
-	
-	
-	
-	
-	
-	
 
 }
